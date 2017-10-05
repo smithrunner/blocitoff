f38a46085aa625b38907ddc912c9e9a091c77d1f
Sam Smith <smithrunner@live.com>
2017-10-04 23:53:26 -0500

    old tasks completed

--- app/scripts/services/Task.js
+++ app/scripts/services/Task.js
@@ -6,12 +6,40 @@
     Task.instance = 0;
 
     Task.add = function(task) {
-      tasks.$add({
-        'task': task
-      });
+      var datetime = new Date();
+      var time = datetime.getTime();
+      var finish_time = time + 604800000
+
+      var newTask = {
+        task_title : task,
+        finish_time : finish_time,
+        finished : 'false'
+      };
+      tasks.$add(newTask);
     };
 
-    Task.all = tasks;
+    Task.checkTime = function(finish_time , finished) {
+        var date = new Date();
+        var time = date.getTime();
+
+        if (finished == 'true') {
+          return true;
+        } else if (time > finish_time) {
+          return true;
+        } else {
+          return false;
+        };
+    };
+
+    Task.complete = function(task) {
+
+      var ref = firebase.database().ref().child("tasks").child(task.$id);
+      var obj = {finished : 'true'}
+      ref.update(obj);
+
+    };
+
+    Task.all_tasks = tasks;
 
     return Task;
   }
